bf5d3810763a8a2fa2725c01d0613a3f5532b226
mimiCrai <13523054@std.stei.itb.ac.id>
2025-08-21 17:56:12 +0700

    fix: db railway

--- src/main.ts
+++ src/main.ts
@@ -111,7 +111,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document); // bisa diakses di /docs
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on port ${port}`);
 }
 bootstrap();
